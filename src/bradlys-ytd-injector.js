2655b36fe4ab276d66a8ac6b54dc1f0d0010c1d7
Bradly Schlenker <bradlyschlenker@gmail.com>
2015-09-06 23:19:40 -0700

    Wrong file name

--- src/bradlys-ytd-injector.js
+++ src/bradlys-ytd-injector.js
@@ -1,5 +1,5 @@
 var s = document.createElement('script');
-s.src = chrome.extension.getURL('dl.js');
+s.src = chrome.extension.getURL('bradlys-ytd.js');
 s.onload = function() {
 	this.parentNode.removeChild(this);
 };
