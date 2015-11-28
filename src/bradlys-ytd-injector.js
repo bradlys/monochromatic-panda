6e91c9f6ccc63f82449f64a1825c11ec77f2b5d4
Bradly Schlenker <bradlyschlenker@gmail.com>
2015-11-27 17:17:22 -0800

    Updated decryption algorithm

--- src/bradlys-ytd-injector.js
+++ src/bradlys-ytd-injector.js
@@ -1,14 +1,138 @@
+function parseDecryptionFunction(url) {
+    var functionString = '';
+    if (!url) {
+        return functionString;
+    }
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (request.readyState == 4 && request.status == 200) {
+            parseScript(url, request.responseText);
+        }
+    };
+    request.open("GET", url);
+    request.send();
+    return '';
+}
+
+function parseScript(url, script){
+    if(!script || !url){
+        return '';
+    }
+    var mainExecutionFunctionName = script.match(/\.set\("signature",(.+)\(/);
+    if(!mainExecutionFunctionName){
+        return '';
+    }
+    mainExecutionFunctionName = mainExecutionFunctionName[1];
+    var mainFunction = parseMethodFromScript(mainExecutionFunctionName, script);
+    if(!mainFunction){
+        return '';
+    }
+    var subFunction = parseSubFunctionFromMethodAndScript(mainFunction, mainExecutionFunctionName, script);
+    if(!subFunction){
+        return '';
+    }
+    var decryptionScheme = 'decrypt_signature = function (sig) { ' +
+                                subFunction + ' ' +
+                                mainFunction +
+                                ' return ' + mainExecutionFunctionName + '(sig);};';
+    var scriptElement = document.createElement('script');
+    scriptElement.innerText = decryptionScheme;
+    scriptElement.onload = function() { this.parentNode.removeChild(this);};
+    (document.head||document.documentElement).appendChild(scriptElement);
+}
+
+/**
+ * Returns the declaration of a method when given the provided method name and haystack to search.
+ *
+ * @param methodName name of the method to search for
+ * @param haystack string to search in
+ * @returns {boolean|string}
+ */
+function parseMethodFromScript(methodName, haystack) {
+    methodName = regexEscapeString(methodName);
+    var methodMatch = haystack.match(new RegExp("(var " + methodName + "={[\\S\\s]*?(?=}};)}};)"), 'm');
+    if(!methodMatch) {
+        methodMatch = haystack.match(new RegExp("(var " + methodName + "=function\\([\\w$]+\\){[^}]*};)", 'm'));
+    }
+    if(!methodMatch) {
+        methodMatch = haystack.match(new RegExp("(function " + methodName + "\\([\\w$]+\\){[^}]*};)", 'm'));
+    }
+    if(!methodMatch) {
+        return false;
+    }
+    return methodMatch[1];
+}
+
+/**
+ * Returns the first parameter of the provided method that is within the haystack.
+ *
+ * @param methodName name of the method to search for
+ * @param haystack string to search in
+ * @returns {boolean|string}
+ */
+function parseMethodParameterFromScript(methodName, haystack) {
+    methodName = regexEscapeString(methodName);
+    var methodMatch = haystack.match(new RegExp("function " + methodName + "\\(([\\w$]+)\\){[^}]*};", 'm'));
+    if(!methodMatch) {
+        methodMatch = haystack.match(new RegExp("var " + methodName + "=function\\(([\\w$]+)\\){[^}]*};", 'm'));
+    }
+    if(!methodMatch) {
+        return false;
+    }
+    return methodMatch[1];
+}
+
+/**
+ * Obtains the first subfunction reference out of the provided method and then
+ * returns that subfunction's declaration.
+ *
+ * @param method method to look for subfunctions in
+ * @param methodName method name of the method provided
+ * @param script script to search
+ * @returns {boolean|string}
+ */
+function parseSubFunctionFromMethodAndScript(method, methodName, script){
+    var methodParameter = parseMethodParameterFromScript(methodName, method);
+    if(!methodParameter){
+        return false;
+    }
+    methodParameter = regexEscapeString(methodParameter);
+    var firstSubFunctionName = method.match(new RegExp("([\\w$]+)\\.[\\w$]+\\(" + methodParameter + "[^)]*\\)", 'm'));
+    if(!firstSubFunctionName){
+        return false;
+    }
+    firstSubFunctionName = firstSubFunctionName[1];
+    var subFunction = parseMethodFromScript(firstSubFunctionName, script);
+    if(!subFunction){
+        return false;
+    }
+    return subFunction;
+}
+
+/**
+ * Take a string and return the regex safe version of it.
+ * Which is to say, a version that can be inserted into regexp as is.
+ *
+ * @param string
+ * @returns {string}
+ */
+function regexEscapeString(string){
+    string = string.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+    return string;
+}
+
 function getDecryptSignature(){
     var url = '';
     var regularPlayer = false;
-    for(var i in document.scripts){
+    var i;
+    for(i in document.scripts){
         if(document.scripts[i].src && document.scripts[i].src.indexOf('html5player') !== -1){
             url = document.scripts[i].src;
             break;
         }
     }
     if(url === ''){
-        for(var i in document.scripts){
+        for(i in document.scripts){
             if(document.scripts[i].src && document.scripts[i].src.indexOf('player') !== -1){
                 url = document.scripts[i].src;
                 regularPlayer = true;
@@ -17,61 +141,6 @@ function getDecryptSignature(){
         }
     }
     parseDecryptionFunction(url);
-    function parseDecryptionFunction(url) {
-        var functionString = '';
-        if (!url) {
-            return functionString;
-        }
-        var request = new XMLHttpRequest();
-        request.onreadystatechange = function() {
-            if (request.readyState == 4 && request.status == 200) {
-                parseScript(url, request.responseText);
-            }
-        };
-        request.open("GET", url);
-        request.send();
-        return '';
-    }
-
-    function parseScript(url, script){
-        if(!script || !url){
-            return '';
-        }
-        var mainExecutionFunctionName = script.match(/\.set\("signature",(.+)\(/);
-        if(!mainExecutionFunctionName){
-            return '';
-        }
-        mainExecutionFunctionName = mainExecutionFunctionName[1];
-        var regexClearMainExecutionFunctionName = mainExecutionFunctionName.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
-        var mainFunction = script.match(new RegExp("(function " + regexClearMainExecutionFunctionName + "\\(([\\w$]+)\\){[^}]*});"));
-        if(!mainFunction) {
-            mainFunction = script.match(new RegExp("(var " + regexClearMainExecutionFunctionName + "\\=function\\(([\\w$]+)\\){[^}]*});"));
-            if(!mainFunction) {
-                return '';
-            }
-        }
-        var notNeedle = mainFunction[2];
-        mainFunction = mainFunction[1];
-        var subFunctions = mainFunction.match(new RegExp("[\\w$]+\\.[\\w$]+\\(" + notNeedle.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&") + "[^)]*\\)", 'g'));
-        if(!subFunctions){
-            return '';
-        }
-        var subFunctionName = subFunctions[0].match(new RegExp('(\\w+)\\.', 'g'))[0].replace('.', '');
-        var regexClearSubFunctionName = subFunctionName.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
-        var subFunction = script.match(new RegExp("(var " + regexClearSubFunctionName + "\\={[^}]*})[\\S\\s]*?(?=}};)}};"));
-        if(!subFunction){
-            subFunction = script.match(new RegExp("(var " + regexClearSubFunctionName + "\\=function\\(([\\w$]+)\\){[^}]*});"));
-            if(!subFunction){
-                return '';
-            }
-        }
-        subFunction = subFunction[0];
-        var decryptionScheme = 'decrypt_signature = function (sig) { ' + subFunction + ' ' + mainFunction + '; return ' + mainExecutionFunctionName + '(sig);};';
-        var scriptElement = document.createElement('script');
-        scriptElement.innerText = decryptionScheme;
-        scriptElement.onload = function() { this.parentNode.removeChild(this);};
-        (document.head||document.documentElement).appendChild(scriptElement);
-    }
 }
 
 getDecryptSignature();
