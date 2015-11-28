883fe3e23c1758022402ab4a7ceb550396aeeba7
Bradly Schlenker <bradlyschlenker@gmail.com>
2015-11-27 19:07:48 -0800

    Pulled videos out of function to make global-ish.

--- src/bradlys-ytd.js
+++ src/bradlys-ytd.js
@@ -100,88 +100,6 @@ function getYouTubeVideos() {
 	if (!ytplayer || !ytplayer.config || !ytplayer.config.args || !ytplayer.config.args.url_encoded_fmt_stream_map) {
 		return null;
 	}
-	//video formats information taken from youtube-dl project
-	//each number corresponds to a unique type of video format
-	var YTVideoFormats = {
-		'5': {'ext': 'flv', 'width': 400, 'height': 240},
-		'6': {'ext': 'flv', 'width': 450, 'height': 270},
-		'13': {'ext': '3gp', 'width': 176, 'height': 144, 'format_note' : 'Mono'},
-		'17': {'ext': '3gp', 'width': 176, 'height': 144},
-		'18': {'ext': 'mp4', 'width': 640, 'height': 360},
-		'22': {'ext': 'mp4', 'width': 1280, 'height': 720},
-		'34': {'ext': 'flv', 'width': 640, 'height': 360},
-		'35': {'ext': 'flv', 'width': 854, 'height': 480},
-		'36': {'ext': '3gp', 'width': 320, 'height': 240},
-		'37': {'ext': 'mp4', 'width': 1920, 'height': 1080},
-		'38': {'ext': 'mp4', 'width': 4096, 'height': 3072},
-		'43': {'ext': 'webm', 'width': 640, 'height': 360},
-		'44': {'ext': 'webm', 'width': 854, 'height': 480},
-		'45': {'ext': 'webm', 'width': 1280, 'height': 720},
-		'46': {'ext': 'webm', 'width': 1920, 'height': 1080},
-		'59': {'ext': 'mp4', 'width': 854, 'height': 480},
-		'78': {'ext': 'mp4', 'width': 854, 'height': 480},
-		//3D videos
-		'82': {'ext': 'mp4', 'height': 360, 'format_note': '3D', 'preference': -20},
-		'83': {'ext': 'mp4', 'height': 480, 'format_note': '3D', 'preference': -20},
-		'84': {'ext': 'mp4', 'height': 720, 'format_note': '3D', 'preference': -20},
-		'85': {'ext': 'mp4', 'height': 1080, 'format_note': '3D', 'preference': -20},
-		'100': {'ext': 'webm', 'height': 360, 'format_note': '3D', 'preference': -20},
-		'101': {'ext': 'webm', 'height': 480, 'format_note': '3D', 'preference': -20},
-		'102': {'ext': 'webm', 'height': 720, 'format_note': '3D', 'preference': -20},
-		//Apple HTTP Live Streaming
-		'92': {'ext': 'mp4', 'height': 240, 'format_note': 'HLS', 'preference': -10},
-		'93': {'ext': 'mp4', 'height': 360, 'format_note': 'HLS', 'preference': -10},
-		'94': {'ext': 'mp4', 'height': 480, 'format_note': 'HLS', 'preference': -10},
-		'95': {'ext': 'mp4', 'height': 720, 'format_note': 'HLS', 'preference': -10},
-		'96': {'ext': 'mp4', 'height': 1080, 'format_note': 'HLS', 'preference': -10},
-		'132': {'ext': 'mp4', 'height': 240, 'format_note': 'HLS', 'preference': -10},
-		'151': {'ext': 'mp4', 'height': 72, 'format_note': 'HLS', 'preference': -10},
-		//DASH mp4 video
-		'133': {'ext': 'mp4', 'height': 240, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
-		'134': {'ext': 'mp4', 'height': 360, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
-		'135': {'ext': 'mp4', 'height': 480, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
-		'136': {'ext': 'mp4', 'height': 720, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
-		'137': {'ext': 'mp4', 'height': 1080, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
-		'138': {'ext': 'mp4', 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
-		'160': {'ext': 'mp4', 'height': 144, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
-		'264': {'ext': 'mp4', 'height': 1440, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
-		'298': {'ext': 'mp4', 'height': 720, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40, 'fps': 60, 'vcodec': 'h264'},
-		'299': {'ext': 'mp4', 'height': 1080, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40, 'fps': 60, 'vcodec': 'h264'},
-		'266': {'ext': 'mp4', 'height': 2160, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40, 'vcodec': 'h264'},
-		//DASH mp4 audio
-		'139': {'ext': 'm4a', 'format_note': 'DASH audio', 'acodec': 'aac', 'vcodec': 'none', 'abr': 48, 'preference': -50, 'container': 'm4a_dash'},
-		'140': {'ext': 'm4a', 'format_note': 'DASH audio', 'acodec': 'aac', 'vcodec': 'none', 'abr': 128, 'preference': -50, 'container': 'm4a_dash'},
-		'141': {'ext': 'm4a', 'format_note': 'DASH audio', 'acodec': 'aac', 'vcodec': 'none', 'abr': 256, 'preference': -50, 'container': 'm4a_dash'},
-		//Dash webm
-		'167': {'ext': 'webm', 'height': 360, 'width': 640, 'format_note': 'DASH video', 'acodec': 'none', 'container': 'webm', 'vcodec': 'vp8', 'preference': -40},
-		'168': {'ext': 'webm', 'height': 480, 'width': 854, 'format_note': 'DASH video', 'acodec': 'none', 'container': 'webm', 'vcodec': 'vp8', 'preference': -40},
-		'169': {'ext': 'webm', 'height': 720, 'width': 1280, 'format_note': 'DASH video', 'acodec': 'none', 'container': 'webm', 'vcodec': 'vp8', 'preference': -40},
-		'170': {'ext': 'webm', 'height': 1080, 'width': 1920, 'format_note': 'DASH video', 'acodec': 'none', 'container': 'webm', 'vcodec': 'vp8', 'preference': -40},
-		'218': {'ext': 'webm', 'height': 480, 'width': 854, 'format_note': 'DASH video', 'acodec': 'none', 'container': 'webm', 'vcodec': 'vp8', 'preference': -40},
-		'219': {'ext': 'webm', 'height': 480, 'width': 854, 'format_note': 'DASH video', 'acodec': 'none', 'container': 'webm', 'vcodec': 'vp8', 'preference': -40},
-		'278': {'ext': 'webm', 'height': 144, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40, 'container': 'webm', 'vcodec': 'vp9'},
-		'242': {'ext': 'webm', 'height': 240, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
-		'243': {'ext': 'webm', 'height': 360, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
-		'244': {'ext': 'webm', 'height': 480, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
-		'245': {'ext': 'webm', 'height': 480, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
-		'246': {'ext': 'webm', 'height': 480, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
-		'247': {'ext': 'webm', 'height': 720, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
-		'248': {'ext': 'webm', 'height': 1080, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
-		'271': {'ext': 'webm', 'height': 1440, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
-		'272': {'ext': 'webm', 'height': 2160, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
-		'302': {'ext': 'webm', 'height': 720, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40, 'fps': 60, 'vcodec': 'vp9'},
-		'303': {'ext': 'webm', 'height': 1080, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40, 'fps': 60, 'vcodec': 'vp9'},
-		'308': {'ext': 'webm', 'height': 1440, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40, 'fps': 60, 'vcodec': 'vp9'},
-		'313': {'ext': 'webm', 'height': 2160, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40, 'vcodec': 'vp9'},
-		'315': {'ext': 'webm', 'height': 2160, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40, 'fps': 60, 'vcodec': 'vp9'},
-		//DASH webm audio
-		'171': {'ext': 'webm', 'vcodec': 'none', 'format_note': 'DASH audio', 'abr': 128, 'preference': -50},
-		'172': {'ext': 'webm', 'vcodec': 'none', 'format_note': 'DASH audio', 'abr': 256, 'preference': -50},
-		//DASH webm audio with opus inside
-		'249': {'ext': 'webm', 'vcodec': 'none', 'format_note': 'DASH audio', 'acodec': 'opus', 'abr': 50, 'preference': -50},
-		'250': {'ext': 'webm', 'vcodec': 'none', 'format_note': 'DASH audio', 'acodec': 'opus', 'abr': 70, 'preference': -50},
-		'251': {'ext': 'webm', 'vcodec': 'none', 'format_note': 'DASH audio', 'acodec': 'opus', 'abr': 160, 'preference': -50}
-	};
 	//grab the videos out of the ytplayer variable
 	var YTPlayerVideos = ytplayer.config.args.url_encoded_fmt_stream_map.split(',');
 	var videos = [];
@@ -278,3 +196,86 @@ function getYouTubeVideos() {
 	regularAndAdaptiveVideos.push(videos);
 	return regularAndAdaptiveVideos;
 }
+
+//video formats information taken from youtube-dl project
+//each number corresponds to a unique type of video format
+var YTVideoFormats = {
+    '5': {'ext': 'flv', 'width': 400, 'height': 240},
+    '6': {'ext': 'flv', 'width': 450, 'height': 270},
+    '13': {'ext': '3gp', 'width': 176, 'height': 144, 'format_note' : 'Mono'},
+    '17': {'ext': '3gp', 'width': 176, 'height': 144},
+    '18': {'ext': 'mp4', 'width': 640, 'height': 360},
+    '22': {'ext': 'mp4', 'width': 1280, 'height': 720},
+    '34': {'ext': 'flv', 'width': 640, 'height': 360},
+    '35': {'ext': 'flv', 'width': 854, 'height': 480},
+    '36': {'ext': '3gp', 'width': 320, 'height': 240},
+    '37': {'ext': 'mp4', 'width': 1920, 'height': 1080},
+    '38': {'ext': 'mp4', 'width': 4096, 'height': 3072},
+    '43': {'ext': 'webm', 'width': 640, 'height': 360},
+    '44': {'ext': 'webm', 'width': 854, 'height': 480},
+    '45': {'ext': 'webm', 'width': 1280, 'height': 720},
+    '46': {'ext': 'webm', 'width': 1920, 'height': 1080},
+    '59': {'ext': 'mp4', 'width': 854, 'height': 480},
+    '78': {'ext': 'mp4', 'width': 854, 'height': 480},
+    //3D videos
+    '82': {'ext': 'mp4', 'height': 360, 'format_note': '3D', 'preference': -20},
+    '83': {'ext': 'mp4', 'height': 480, 'format_note': '3D', 'preference': -20},
+    '84': {'ext': 'mp4', 'height': 720, 'format_note': '3D', 'preference': -20},
+    '85': {'ext': 'mp4', 'height': 1080, 'format_note': '3D', 'preference': -20},
+    '100': {'ext': 'webm', 'height': 360, 'format_note': '3D', 'preference': -20},
+    '101': {'ext': 'webm', 'height': 480, 'format_note': '3D', 'preference': -20},
+    '102': {'ext': 'webm', 'height': 720, 'format_note': '3D', 'preference': -20},
+    //Apple HTTP Live Streaming
+    '92': {'ext': 'mp4', 'height': 240, 'format_note': 'HLS', 'preference': -10},
+    '93': {'ext': 'mp4', 'height': 360, 'format_note': 'HLS', 'preference': -10},
+    '94': {'ext': 'mp4', 'height': 480, 'format_note': 'HLS', 'preference': -10},
+    '95': {'ext': 'mp4', 'height': 720, 'format_note': 'HLS', 'preference': -10},
+    '96': {'ext': 'mp4', 'height': 1080, 'format_note': 'HLS', 'preference': -10},
+    '132': {'ext': 'mp4', 'height': 240, 'format_note': 'HLS', 'preference': -10},
+    '151': {'ext': 'mp4', 'height': 72, 'format_note': 'HLS', 'preference': -10},
+    //DASH mp4 video
+    '133': {'ext': 'mp4', 'height': 240, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
+    '134': {'ext': 'mp4', 'height': 360, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
+    '135': {'ext': 'mp4', 'height': 480, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
+    '136': {'ext': 'mp4', 'height': 720, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
+    '137': {'ext': 'mp4', 'height': 1080, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
+    '138': {'ext': 'mp4', 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
+    '160': {'ext': 'mp4', 'height': 144, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
+    '264': {'ext': 'mp4', 'height': 1440, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
+    '298': {'ext': 'mp4', 'height': 720, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40, 'fps': 60, 'vcodec': 'h264'},
+    '299': {'ext': 'mp4', 'height': 1080, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40, 'fps': 60, 'vcodec': 'h264'},
+    '266': {'ext': 'mp4', 'height': 2160, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40, 'vcodec': 'h264'},
+    //DASH mp4 audio
+    '139': {'ext': 'm4a', 'format_note': 'DASH audio', 'acodec': 'aac', 'vcodec': 'none', 'abr': 48, 'preference': -50, 'container': 'm4a_dash'},
+    '140': {'ext': 'm4a', 'format_note': 'DASH audio', 'acodec': 'aac', 'vcodec': 'none', 'abr': 128, 'preference': -50, 'container': 'm4a_dash'},
+    '141': {'ext': 'm4a', 'format_note': 'DASH audio', 'acodec': 'aac', 'vcodec': 'none', 'abr': 256, 'preference': -50, 'container': 'm4a_dash'},
+    //Dash webm
+    '167': {'ext': 'webm', 'height': 360, 'width': 640, 'format_note': 'DASH video', 'acodec': 'none', 'container': 'webm', 'vcodec': 'vp8', 'preference': -40},
+    '168': {'ext': 'webm', 'height': 480, 'width': 854, 'format_note': 'DASH video', 'acodec': 'none', 'container': 'webm', 'vcodec': 'vp8', 'preference': -40},
+    '169': {'ext': 'webm', 'height': 720, 'width': 1280, 'format_note': 'DASH video', 'acodec': 'none', 'container': 'webm', 'vcodec': 'vp8', 'preference': -40},
+    '170': {'ext': 'webm', 'height': 1080, 'width': 1920, 'format_note': 'DASH video', 'acodec': 'none', 'container': 'webm', 'vcodec': 'vp8', 'preference': -40},
+    '218': {'ext': 'webm', 'height': 480, 'width': 854, 'format_note': 'DASH video', 'acodec': 'none', 'container': 'webm', 'vcodec': 'vp8', 'preference': -40},
+    '219': {'ext': 'webm', 'height': 480, 'width': 854, 'format_note': 'DASH video', 'acodec': 'none', 'container': 'webm', 'vcodec': 'vp8', 'preference': -40},
+    '278': {'ext': 'webm', 'height': 144, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40, 'container': 'webm', 'vcodec': 'vp9'},
+    '242': {'ext': 'webm', 'height': 240, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
+    '243': {'ext': 'webm', 'height': 360, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
+    '244': {'ext': 'webm', 'height': 480, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
+    '245': {'ext': 'webm', 'height': 480, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
+    '246': {'ext': 'webm', 'height': 480, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
+    '247': {'ext': 'webm', 'height': 720, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
+    '248': {'ext': 'webm', 'height': 1080, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
+    '271': {'ext': 'webm', 'height': 1440, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
+    '272': {'ext': 'webm', 'height': 2160, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40},
+    '302': {'ext': 'webm', 'height': 720, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40, 'fps': 60, 'vcodec': 'vp9'},
+    '303': {'ext': 'webm', 'height': 1080, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40, 'fps': 60, 'vcodec': 'vp9'},
+    '308': {'ext': 'webm', 'height': 1440, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40, 'fps': 60, 'vcodec': 'vp9'},
+    '313': {'ext': 'webm', 'height': 2160, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40, 'vcodec': 'vp9'},
+    '315': {'ext': 'webm', 'height': 2160, 'format_note': 'DASH video', 'acodec': 'none', 'preference': -40, 'fps': 60, 'vcodec': 'vp9'},
+    //DASH webm audio
+    '171': {'ext': 'webm', 'vcodec': 'none', 'format_note': 'DASH audio', 'abr': 128, 'preference': -50},
+    '172': {'ext': 'webm', 'vcodec': 'none', 'format_note': 'DASH audio', 'abr': 256, 'preference': -50},
+    //DASH webm audio with opus inside
+    '249': {'ext': 'webm', 'vcodec': 'none', 'format_note': 'DASH audio', 'acodec': 'opus', 'abr': 50, 'preference': -50},
+    '250': {'ext': 'webm', 'vcodec': 'none', 'format_note': 'DASH audio', 'acodec': 'opus', 'abr': 70, 'preference': -50},
+    '251': {'ext': 'webm', 'vcodec': 'none', 'format_note': 'DASH audio', 'acodec': 'opus', 'abr': 160, 'preference': -50}
+};
